b02102a86d888bc6431f953d9a1a5a27e5a47fe5
Ariyo Israel Gbemisola <igbemisola53@gmail.com>
2024-11-08 19:45:14 +0100

    urls updated for ListView

--- news/urls.py
+++ news/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import NewsListView
 
 router = DefaultRouter()
 router.register(r'articles', views.ArticleViewSet)
 router.register(r'comments', views.CommentViewSet)
 
 urlpatterns = [
-    path('', views.news_list, name='news_list'),
+     path('', NewsListView.as_view(), name='news_list'),
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),
     path('api/', include(router.urls)),
     path('fetch/', views.fetch_news, name='fetch_news'),
